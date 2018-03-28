7f9ea9d8bac5f8602ddf741caee7cf68480ae924
Viktor Kacenak <vkacenak@gmail.com>
2018-03-28 10:24:04 +0200

    kazdorocne aktivity

--- kazdorocneaktivity.php
+++ kazdorocneaktivity.php
@@ -20,6 +20,40 @@
 </div>
 </div>
 
+
+<div class="row">
+<div class="aktivity-item bgNRCC" id="aktivity-kalamajky">
+<div class="aktivity-overlay"></div><h4 class="w600">KALAMAJKY</h4></div>
+<div class="aktivity-content aktivity-kalamajky">
+  <div class="row">
+
+    <div class="col-md-6">
+      <h2 class="w700">KALAMAJKY</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">KOŽaZ (kurz na ochranu života a zdravia) je určený žiakom 3. ročníka a je súčasťou plánu
+práce školy. Účasť žiakov na kurze je povinná. Obsahovými štandardami sú teoretická
+príprava, praktický výcvik a mimo vyučovacie aktivity. Kurz sa realizuje internátnou formou v
+Škole v prírode v Kysaku a dennou dochádzkou v Košiciach a ich okolí. Cieľom KOŽaZ-u je
+zvýšiť telesnú zdatnosť žiakov, psychickú odolnosť, zvýšiť sebavedomie a pripravenosť riešiť
+mimoriadne situácie v živote, v praxi aplikovať zdravotnícku prípravu.
+Najzaujímavejšou časťou je pobyt v prírode, kde sú žiaci vedení k ochrane, úprave a čistote prírodného
+prostredia, k poznávaniu prírodných úkazov, liečivých a chránených rastlín. Súčasťou praktickej časti sú
+outdorové aktivity – splav Hornádu, lezenie na Jánošíkovej bašte, lukostreľba, streľba zo vzduchových
+zbraní, jazda na koni, ale aj stavanie ohniska a príprava jedla na ohni. Pri obľúbenej nočnej hre si žiaci
+precvičujú vedomosti a zručnosti, ktoré nadobudli na stanovištiach na Účelovom cvičení v 1. a 2. ročníku,
+upevňujú si psychickú odolnosť a  učia sa kooperovať v malých skupinách.
+</p>
+
+</div>
+
+<div class="col-md-5"><img src="img/kazdorocneAktivity/kala2.jpg">
+</div>
+</div>
+
+
+</div>
+</div>
+
+
+
 <div class="row">
 <div class="aktivity-item bgNRCC" id="aktivity-kozaz">
 <div class="aktivity-overlay"></div><h4 class="w600">KOŽAZ</h4></div>
@@ -27,8 +61,18 @@
   <div class="row">
 
     <div class="col-md-6">
-      <h2 class="w700">KOŽAZ</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      <h2 class="w700">KOŽAZ</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">KOŽaZ (kurz na ochranu života a zdravia) je určený žiakom 3. ročníka a je súčasťou plánu
+práce školy. Účasť žiakov na kurze je povinná. Obsahovými štandardami sú teoretická
+príprava, praktický výcvik a mimo vyučovacie aktivity. Kurz sa realizuje internátnou formou v
+Škole v prírode v Kysaku a dennou dochádzkou v Košiciach a ich okolí. Cieľom KOŽaZ-u je
+zvýšiť telesnú zdatnosť žiakov, psychickú odolnosť, zvýšiť sebavedomie a pripravenosť riešiť
+mimoriadne situácie v živote, v praxi aplikovať zdravotnícku prípravu.
+Najzaujímavejšou časťou je pobyt v prírode, kde sú žiaci vedení k ochrane, úprave a čistote prírodného
+prostredia, k poznávaniu prírodných úkazov, liečivých a chránených rastlín. Súčasťou praktickej časti sú
+outdorové aktivity – splav Hornádu, lezenie na Jánošíkovej bašte, lukostreľba, streľba zo vzduchových
+zbraní, jazda na koni, ale aj stavanie ohniska a príprava jedla na ohni. Pri obľúbenej nočnej hre si žiaci
+precvičujú vedomosti a zručnosti, ktoré nadobudli na stanovištiach na Účelovom cvičení v 1. a 2. ročníku,
+upevňujú si psychickú odolnosť a  učia sa kooperovať v malých skupinách.
 </p>
 
 </div>
@@ -48,12 +92,27 @@
 <div class="aktivity-content aktivity-stuzkova">  <div class="row">
 
     <div class="col-md-6">
-      <h2 class="w700">STUŽKOVÁ</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      <h2 class="w700">STUŽKOVÁ</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">„Obleky, kravaty profesor dojatý pri pulte on a ty, dievčatá zmenené šatami na
+dámy, úsmevné spomienky - už je to za nami!“
+Aj takto znie časť textu obľúbenej piesne, ktorá je neodmysliteľne spätá so
+stužkovými slávnosťami. Všetky elementy, ktoré známa skupina vo svojej
+skladbe použila, sú sprevádzané aj silnými emóciami zo strany študentov,
+rodičov, učiteľov.
+Každoročne stoja žiaci štvrtých ročníkov pred bránami dospelosti a čakajú na
+zelenú stužku nádeje, ktorá je ich prvým symbolickým krokom medzi
+„dospelákov“. Slávnostný akt prijímania stužiek sa koná v Dome umenia.
+
+Tradíciou nášho gymnázia je spoločná slávnosť pre žiakov všetkých štvrtáckych
+tried. Večer prebieha v slávnostnej atmosfére, je rozdelený na dve časti.
+Oficiálna časť sa koná v koncertnej sále, jej hlavnými aktérmi sú samotní
+štvrtáci, medziprogram dopĺňajú vystúpenia žiakov nižších ročníkov. Druhá časť
+je venovaná tancu, hudbe - jednoducho zábave!
+Stužková slávnosť je súčasťou života každého mladého človeka a krásne
+spomienky, na čas rozlúčky so strednou školou, ostanú v pamäti navždy!
 </p>
 
 </div>
-<div class="col-md-5"><a href="img/kazdorocneAktivity/stuzkova1.jpg" data-toggle="lightbox"><img src="img/kazdorocneAktivity/stuzkova1.jpg"></a>
+<div class="col-md-5"><img src="img/kazdorocneAktivity/stuzkova1.jpg">
 </div>
 </div>
 </div>
@@ -66,16 +125,115 @@
 <div class="aktivity-content aktivity-lyziarsky">  <div class="row">
 
     <div class="col-md-6">
-      <h2 class="w700">LYŽIARSKY VÝCVIK</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+      <h2 class="w700">LYŽIARSKY VÝCVIK</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">Na našom gymnáziu organizujeme aj lyžiarske výlety, ktoré sú určené žiakom
+navštevujúcim športový krúžok. Najčastejšie sa táto jednodňová lyžovačka koná na
+zjazdovkách Vysokých Tatier.
+Nový rozmer a nezabudnuteľný zážitok ponúka zahraničný lyžiarsky pobyt
+v talianskych Dolomitoch, ktorý býva naplánovaný na koniec januára. Stredisko Passo
+Oclini sa nachádza v prekrásnom vysokohorskom prostredí, má ideálnu polohu
+v horskom sedle, vo výške 2000 m. n. m. Žiaci si užijú lyžovanie na perfektne
+upravených poloprázdnych zjazdovkách rôznych náročností. Hotel je situovaný priamo
+pod zjazdovkami. Žiakom sú k dispozícii aj oddychové zóny hotela s možnosťou
+športového a kultúrneho vyžitia sa. K zaujímavému pobytu patrí aj kvalitná
+gastronómia,  preto sa milovníci dobrého talianskeho jedla majú na čo tešiť a prídu si
+na svoje.
 </p>
 
 </div>
-<div class="col-md-5"><a href="img/kazdorocneAktivity/lyziarsky1.jpg" data-toggle="lightbox"><img src="img/kazdorocneAktivity/lyziarsky1.jpg"></a>
+<div class="col-md-5"><img src="img/kazdorocneAktivity/lyziarsky1.jpg">
 </div>
 </div>
 </div>
 </div>
+
+<div class="row">
+<div class="aktivity-item bgNRCC" id="aktivity-enviromentalnyt">
+<div class="aktivity-overlay"></div><h4 class="w600">ENVIROMENTÁLNY TÝŽDEŇ</h4></div>
+<div class="aktivity-content aktivity-enviromentalnyt">
+  <div class="row">
+
+    <div class="col-md-6">
+      <h2 class="w700">ENVIROMENTÁLNY TÝŽDEŇ</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">Deň Zeme je venovaný našej planéte a v kalendári mu každoročne patrí dátum 22.
+apríl. Na Šrobárke si ho pripomíname rôznymi aktivitami a aprílový týždeň, na ktorý
+sú naplánované, sme nazvali environmentálny. Konkrétne ide o obdobie od 23. do 27.
+apríla. Cieľom tejto aktivity je zlepšenie energetickej efektívnosti, recyklovanie
+odpadov a hľadanie obnoviteľných zdrojov energie. Vďaka tomu žiaci získajú nielen
+vedomosti, ale aj zručnosti. Jednoduchými činnosťami veľmi radi pomáhajú
+ozdravovať naše životné prostredie.
+</p>
+
+</div>
+
+<div class="col-md-5"><img src="img/kazdorocneAktivity/eco2.jpg">
+</div>
+</div>
+
+
+</div>
+</div>
+
+<div class="row">
+<div class="aktivity-item bgNRCC" id="aktivity-protidrogovyt">
+<div class="aktivity-overlay"></div><h4 class="w600">PROTIDROGOVÝ TÝŽDEŇ</h4></div>
+<div class="aktivity-content aktivity-protidrogovyt">
+  <div class="row">
+
+    <div class="col-md-6">
+      <h2 class="w700">PROTIDROGOVÝ TÝŽDEŇ</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">
+        Predprázdninový týždeň na našom gymnáziu patrí rôznorodým aktivitám. Do povedomia žiakov
+a učiteľov sa zapísal ako Protidrogový týždeň. Blížia sa prázdniny, čas voľna, a preto sa organizátori
+snažia žiakov inšpirovať a motivovať k aktivitám, ktorými by mohli predchádzať prípadným
+nebezpečným závislostiam. Projekt sa uskutočňuje už veľmi dlho a patrí k šrobárskym tradičným
+podujatiam. Dušou a organizátormi sú hlavne vyučujúci výchovných predmetov, ale na organizácii sa
+podieľa každá predmetová komisia. K najpopulárnejším patria športové súťaže. Žiaci zabojujú vo
+volejbalovom, futbalovom či basketbalovom turnaji. Pre divákov je veľkou atrakciou zápas víťazného
+žiackeho tímu s tímom učiteľov. Veľkej popularite sa teší aj splavovanie Hornádu a adrenalínové
+lezenie po našej bouldrovej stene. Aj keď sme škola v srdci Košíc, príležitostí na pohyb je v nej dosť.
+Milovníci a obdivovatelia kultivovanejšieho pohybu môžu v tomto týždni získať základy štandardných
+alebo latinsko-amerických spoločenských tancov. Súčasťou Protidrogového týždňa sú aj besedy
+s umelcami, spisovateľmi, športovcami a prednášky na témy podľa žiackeho výberu. Tí, ktorí majú
+záujem o naše Košice, si tiež prídu na svoje. S fundovanými sprievodcami sa počas exkurzií
+dostaneme na miesta, ktoré sú mimoriadne zaujímavé a bežnému človeku nedostupné. No a umelci-
+výtvarníci? Aj pre nich sa utvoril priestor a realizujú sa kresbou, maľbou, tvorením koláží či
+zaujímavých ručných prác. Na našom gymnáziu študujú aj žiaci, ktorí si aj tento týždeň radi preveria
+svoje schopnosti a vedomosti. Zapájajú sa do šachového turnaja či súťaže v Scrabble, prezrú si filmy
+v cudzom jazyku a pod. Každý rok si počas obľúbeného Protidrogového týždňa máme z čoho vyberať.
+</p>
+
+</div>
+
+<div class="col-md-5"><img src="img/kazdorocneAktivity/protidrogovyt.jpg">
+</div>
+</div>
+
+
+</div>
+</div>
+
+<div class="row">
+
+<div class="aktivity-item bgNRCC" id="aktivity-zberpapiera">
+<div class="aktivity-overlay"></div><h4 class="w600">ZBER PAPIERA</h4></div>
+<div class="aktivity-content aktivity-zberpapiera">
+  <div class="row">
+
+    <div class="col-md-6">
+      <h2 class="w700">ZBER PAPIERA</h2> </div></div> <div class="row"><div class="col-md-6"><p class="p3">
+
+Na našom gymnáziu sa každoročne koná zber papiera, pri ktorom sa snažíme žiakov motivovať k ochrane životného prostredia a podporovať ich environmentálne uvedomenie. Túto akciu má na starosti vybraná trieda, pod vedením pedagóga,  ktorá koordinuje  priebeh zberu. Na školskom dvore je pristavený veľký kontajner, do ktorého sa ukladá vyzbieraný papier. Do tejto aktivity sa pravidelne zapájajú nielen žiaci a ich rodičia, ale aj učitelia a nepedagogickí  pracovníci. Takýmto ekologickým spôsobom sa na recykláciu dostávajú nepotrebné  kopy starých písomiek, obalový materiál a iné veci z papiera. Trieda, ktorá nazbiera najviac kilogramov papiera a tým v najväčšej miere pomôže pri regenerácii životného prostredia, je ocenená obľúbeným  dňom voľna.
+
+</p>
+
+</div>
+
+<div class="col-md-5"><img src="img/kazdorocneAktivity/zber2.jpg">
+</div>
+</div>
+
+
+</div>
+</div>
+
 
           </div> <!-- KONIEC HLAVNEHO CONTAINERU -->
 
@@ -95,6 +253,24 @@
           $(".aktivity-lyziarsky").toggleClass("aktivity-active");
             $(".aktivity-overlay", this).toggleClass("aktivity-overlay-active");
       });
+      $("#aktivity-protidrogovyt").click(function(){
+          $(".aktivity-protidrogovyt").toggleClass("aktivity-active");
+            $(".aktivity-overlay", this).toggleClass("aktivity-overlay-active");
+      });
+
+      $("#aktivity-enviromentalnyt").click(function(){
+          $(".aktivity-enviromentalnyt").toggleClass("aktivity-active");
+            $(".aktivity-overlay", this).toggleClass("aktivity-overlay-active");
+      });
+            $("#aktivity-kalamajky").click(function(){
+                $(".aktivity-kalamajky").toggleClass("aktivity-active");
+                  $(".aktivity-overlay", this).toggleClass("aktivity-overlay-active");
+                  });
+                  $("#aktivity-zberpapiera").click(function(){
+                      $(".aktivity-zberpapiera").toggleClass("aktivity-active");
+                        $(".aktivity-overlay", this).toggleClass("aktivity-overlay-active");
+      });
+
 
   });
 </script>
