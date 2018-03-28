634afff42a31dd324a47e80affa45210a4ade02b
Viktor Kacenak <vkacenak@gmail.com>
2018-03-28 10:24:15 +0200

    p. ponuky

---
+++ pracovneponuky.php
@@ -0,0 +1,24 @@
+
+<?php include 'navbar.html';?>
+
+<body>
+  <div class="photo-heading heading-aktivity bgNRCC">
+    <div class="heading-wrapper">
+      <h1 class="w700 ">Pracovné ponuky</h1>
+
+    </div>
+  </div>
+  <div class="container">
+  <div class="row notfound">
+  <div class="col-lg-2 col-lg-offset-2">
+    <img src="img/pracovneponuky.png">
+    </div>
+    <div class="col-lg-6">
+
+      <p class="p1 w600">Je nám ľúto, ale momentálne sa na našom gymnáziu nenachádzajú žiadne voľné pracovné miesta.</p>
+  </div>
+  </div>
+  </div>
+  </div> <!-- KONIEC HLAVNEHO CONTAINERU -->
+
+<?php include 'footer.html';?>
